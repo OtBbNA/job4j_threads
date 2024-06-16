5d2c18bd67c2f1ec413c051e8578b411ceea7779
OtBbNA <tosha.kostylev.97@mail.ru>
2024-06-16 16:37:48 +0300

    3. Состояние нити. [#229175]

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,23 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        first.start();
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED
+                && second.getState() != Thread.State.TERMINATED) {
+            System.out.println("first thread - " + first.getState());
+            System.out.println("second thread - " + second.getState());
+        }
+        System.out.println("----post cycle state----");
+        System.out.println("first thread - " + first.getState());
+        System.out.println("second thread - " + second.getState());
+        System.out.println("Threads is terminated");
+    }
+}
