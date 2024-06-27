b4c6e586bf352913c72388903d1471d317bc8f8d
OtBbNA <tosha.kostylev.97@mail.ru>
2024-06-27 16:50:12 +0300

    4. Thread без общих ресурсов [#267919]

---
+++ src/main/java/ru/job4j/ref/UserCache.java
@@ -0,0 +1,27 @@
+package ru.job4j.ref;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class UserCache {
+    private final ConcurrentHashMap<Integer, User> users = new ConcurrentHashMap<>();
+    private final AtomicInteger id = new AtomicInteger();
+
+    public void add(User user) {
+        users.put(id.incrementAndGet(), User.of(user.getName()));
+    }
+
+    public User findById(int id) {
+        return User.of(users.get(id).getName());
+    }
+
+    public List<User> findAll() {
+        List<User> rsl = new ArrayList<>();
+        for (int i = 1; i <= id.get(); i++) {
+            rsl.add(findById(i));
+        }
+        return rsl;
+    }
+}
