f2f15042d61a1f4da30deae563741445172bc4aa
OtBbNA <tosha.kostylev.97@mail.ru>
2024-07-06 00:02:13 +0300

    2. Обеспечить остановку потребителя. [#66825]

---
+++ src/main/java/ru/job4j/waitnotify/ParallelSearch.java
@@ -0,0 +1,39 @@
+package ru.job4j.waitnotify;
+
+public class ParallelSearch {
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        for (int index = 0; index != 3; index++) {
+                            try {
+                                queue.offer(index);
+                            } catch (InterruptedException e) {
+                                throw new RuntimeException(e);
+                            }
+                            try {
+                                Thread.sleep(500);
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }
+        ).start();
+    }
+}
