96777f397bf4fddc71de28dfcdf179c96b8649fd
OtBbNA <tosha.kostylev.97@mail.ru>
2024-07-08 01:11:55 +0300

    1. Неблокирующий кеш [#4741]

---
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -0,0 +1,38 @@
+package ru.job4j.cache;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
+
+public class Cache {
+    private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
+
+    public boolean add(Base model) {
+        return memory.putIfAbsent(model.id(), model) == null;
+    }
+
+    public boolean update(Base model) throws OptimisticException {
+        Base stored = memory.get(model.id());
+        if (stored.version() != model.version()) {
+            throw new OptimisticException("Versions are not equal");
+        }
+        return memory.computeIfPresent(model.id(), (key, value) -> {
+                    Base base = new Base(value.id(), model.name(), value.version() + 1);
+                    memory.replace(key, base);
+                    return base;
+                }
+        ) != null;
+    }
+
+    public void delete(int id) {
+        memory.remove(id);
+    }
+
+    public Optional<Base> findById(int id) {
+        return Stream.of(memory.get(id))
+                .filter(Objects::nonNull)
+                .findFirst();
+    }
+}
