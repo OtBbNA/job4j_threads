c1fc312c2aa7cc35aceb61fcc895c21243bc1b2c
OtBbNA <tosha.kostylev.97@mail.ru>
2024-07-08 17:13:18 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/pool/EmailNotification.java
@@ -0,0 +1,30 @@
+package ru.job4j.pool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import net.jcip.annotations.ThreadSafe;
+
+@ThreadSafe
+public class EmailNotification {
+
+    private ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
+
+    public synchronized void emailTo(User user) {
+        pool.submit(() -> {
+                send(user.username(),
+                        String.format("subject = Notification %s, to email %s. \nbody = Add a new event to %s%n",
+                                user.username(), user.email(), user.username()),
+                        user.email());
+                }
+        );
+    }
+
+    public synchronized void close() {
+        pool.shutdown();
+    }
+
+    public void send(String subject, String body, String email) {
+    }
+}
