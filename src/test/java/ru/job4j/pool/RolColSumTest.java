3450cc4680b5a9e6fbf77643aa2dd78ca781f359
OtBbNA <tosha.kostylev.97@mail.ru>
2024-07-11 03:17:28 +0300

    4. CompletableFuture [#361626 #504696]

--- src/test/java/ru/job4j/pool/RolColSumTest.java
+++ src/test/java/ru/job4j/pool/RolColSumTest.java
@@ -12,7 +12,7 @@ class RolColSumTest {
     @Test
     void asyncSum() throws ExecutionException, InterruptedException {
         long timeMillis = System.currentTimeMillis();
-        int[][] matrix = new int[][]{{1, 7, 2},{8, 1, 3},{0, 2, 4}};
+        int[][] matrix = new int[][]{{1, 7, 2}, {8, 1, 3}, {0, 2, 4}};
         int[] expected = new int[]{10, 9, 12, 10, 6, 9};
         RolColSum.Sums[] rsl = RolColSum.asyncSum(matrix);
         for (int i = 0; i < rsl.length; i++) {
@@ -25,7 +25,7 @@ class RolColSumTest {
     @Test
     void mathSum() {
         long timeMillis = System.currentTimeMillis();
-        int[][] matrix = new int[][]{{1, 7, 2},{8, 1, 3},{0, 2, 4}};
+        int[][] matrix = new int[][]{{1, 7, 2}, {8, 1, 3}, {0, 2, 4}};
         int[] expected = new int[]{10, 9, 12, 10, 6, 9};
         RolColSum.Sums[] rsl = RolColSum.sum(matrix);
         for (int i = 0; i < rsl.length; i++) {
